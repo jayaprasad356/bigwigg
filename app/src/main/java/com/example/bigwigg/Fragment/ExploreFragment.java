3df89a60733a1204f53fb37dc353aad2eb078dac
jayaprasad356 <jayaprasad356@gmail.com>
2022-03-13 18:41:53 +0530

    Initial commit

---
+++ app/src/main/java/com/example/bigwigg/Fragment/ExploreFragment.java
@@ -0,0 +1,32 @@
+package com.example.bigwigg.Fragment;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.bigwigg.R;
+
+public class ExploreFragment extends Fragment {
+
+
+
+
+    public ExploreFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+       View rootview = inflater.inflate(R.layout.fragment_explore, container, false);
+
+       return rootview;
+
+    }
+}
---
+++ app/src/main/java/com/example/bigwigg/MainActivity.java
@@ -0,0 +1,85 @@
+package com.example.bigwigg;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.example.bigwigg.Fragment.BusinessFragment;
+import com.example.bigwigg.Fragment.ExploreFragment;
+import com.example.bigwigg.Fragment.FavouriteFragment;
+import com.example.bigwigg.Fragment.NotificationFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
+
+public class MainActivity extends AppCompatActivity implements NavigationBarView.OnItemSelectedListener {
+
+    BottomNavigationView bottomNavigationView;
+    ExploreFragment exploreFragment;
+    BusinessFragment businessFragment;
+    FavouriteFragment favouriteFragment;
+    NotificationFragment notificationFragment;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        bottomNavigationView.setSelectedItemId(R.id.explore);
+        bottomNavigationView.setOnItemSelectedListener(this);
+
+
+        exploreFragment = new ExploreFragment();
+        businessFragment = new BusinessFragment();
+        favouriteFragment = new FavouriteFragment();
+        notificationFragment = new NotificationFragment();
+        getSupportFragmentManager().beginTransaction().replace(R.id.f1fragment, exploreFragment).commit();
+
+
+
+
+
+
+
+
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+        switch (item.getItemId()) {
+            case R.id.explore:
+                getSupportFragmentManager().beginTransaction().replace(R.id.f1fragment, exploreFragment).commit();
+                return true;
+
+            case R.id.business:
+                getSupportFragmentManager().beginTransaction().replace(R.id.f1fragment,businessFragment ).commit();
+                return true;
+
+
+
+            case R.id.favourite:
+                getSupportFragmentManager().beginTransaction().replace(R.id.f1fragment,favouriteFragment ).commit();
+                return true;
+
+
+            case R.id.notification:
+
+                getSupportFragmentManager().beginTransaction().replace(R.id.f1fragment,notificationFragment ).commit();
+                return true;
+
+
+
+
+
+
+        }
+
+        return false;
+    }
+}
