6529cd09b7937e0d94794c7a488bba1b4f7f1ae8
jayaprasad356 <jayaprasad356@gmail.com>
2022-03-17 22:47:21 +0530

    design updated

--- app/src/main/java/com/example/bigwigg/LoginActivity.java
+++ app/src/main/java/com/example/bigwigg/LoginActivity.java
@@ -2,13 +2,26 @@ package com.example.bigwigg;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
+    Button Loginbtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        Loginbtn = findViewById(R.id.loginbtn);
+        Loginbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LoginActivity.this,MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
