adbf7fd44eb613479c90ffd6e9b720b9ea3a7b80
jayaprasad356 <jayaprasad356@gmail.com>
2022-03-31 17:49:38 +0530

    firebase connected and google sign in added

--- app/src/main/java/com/example/bigwigg/LoginActivity.java
+++ app/src/main/java/com/example/bigwigg/LoginActivity.java
@@ -7,22 +7,42 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.firebase.auth.FirebaseAuth;
+
 public class LoginActivity extends AppCompatActivity {
-    Button Loginbtn;
+    Button googlebtn;
+    FirebaseAuth firebaseAuth;
+    GoogleSignInClient mSignInClient;
+    private static int RC_SIGN_IN = 100;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        googlebtn = findViewById(R.id.googlebtn);
+        firebaseAuth = FirebaseAuth.getInstance();
 
-        Loginbtn = findViewById(R.id.loginbtn);
-        Loginbtn.setOnClickListener(new View.OnClickListener() {
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.webclient))
+                .requestEmail()
+                .build();
+        mSignInClient = GoogleSignIn.getClient(this,gso);
+        googlebtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(LoginActivity.this,MainActivity.class);
-                startActivity(intent);
+                  signIn();
+//                Intent intent = new Intent(LoginActivity.this,MainActivity.class);
+//                startActivity(intent);
             }
         });
+    }
+
+    private void signIn() {
+        Intent signInIntent = mSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent,RC_SIGN_IN);
     }
 
     public void googleicon(View view) {
