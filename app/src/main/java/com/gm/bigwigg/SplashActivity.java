9ef2f94db7b71201eb89c781bfdc66d1a666337b
jayaprasad356 <jayaprasad356@gmail.com>
2022-06-16 10:56:36 +0530

    MINOR BUG FIXED

--- app/src/main/java/com/gm/bigwigg/SplashActivity.java
+++ app/src/main/java/com/gm/bigwigg/SplashActivity.java
@@ -2,7 +2,10 @@ package com.gm.bigwigg;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.animation.Animation;
@@ -10,6 +13,13 @@ import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
 
 import com.gm.bigwigg.helper.Session;
+import com.google.android.play.core.appupdate.AppUpdateInfo;
+import com.google.android.play.core.appupdate.AppUpdateManager;
+import com.google.android.play.core.appupdate.AppUpdateManagerFactory;
+import com.google.android.play.core.install.model.AppUpdateType;
+import com.google.android.play.core.install.model.UpdateAvailability;
+import com.google.android.play.core.tasks.OnFailureListener;
+import com.google.android.play.core.tasks.Task;
 
 public class SplashActivity extends AppCompatActivity {
     ImageView Logo;
@@ -28,12 +38,61 @@ public class SplashActivity extends AppCompatActivity {
         Animation myFadeInAnimation = AnimationUtils.loadAnimation(this, R.anim.fade_in);
         Logo.startAnimation(myFadeInAnimation);
 
+        AppUpdateManager appUpdateManager = AppUpdateManagerFactory.create(this);
+        Task<AppUpdateInfo> appUpdateInfoTask = appUpdateManager.getAppUpdateInfo();
+        appUpdateInfoTask.addOnSuccessListener(appUpdateInfo -> {
+
+            if (appUpdateInfo.updateAvailability() == UpdateAvailability.UPDATE_AVAILABLE && appUpdateInfo.isUpdateTypeAllowed(AppUpdateType.IMMEDIATE)) {
+                updateAlertDialog();
+            }
+            else {
+
+                GotoActivity();
+
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(Exception e) {
+                GotoActivity();
+
+            }
+        });
+
+
 
     }
+    private void updateAlertDialog()
+    {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Yep! Here you can see a new update ");
+        builder.setMessage(
+                "We are working hard for your better experience " +
+                        "\n" +
+                        "So, Update it and enter in a new and updated version " +
+                        "\n");
+        builder.setCancelable(false);
 
-    @Override
-    protected void onStart() {
-        super.onStart();
+        builder.setPositiveButton("Update", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("market://details?id="+getPackageName())));
+
+            }
+        });
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Intent intent = new Intent(Intent.ACTION_MAIN);
+                intent.addCategory(Intent.CATEGORY_HOME);
+                startActivity(intent);
+                finish();
+            }
+        });
+        builder.show();
+    }
+
+    private void GotoActivity()
+    {
         Session session = new Session(SplashActivity.this);
         handler.postDelayed(new Runnable() {
             @Override
@@ -54,5 +113,12 @@ public class SplashActivity extends AppCompatActivity {
 
             }
         },3000);
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
     }
 }
